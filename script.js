9737cb353f325cdbbc9b50db893e47c074544698
Dawid Rawicki <113137716+iHejvix@users.noreply.github.com>
2024-04-24 14:35:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 let list = [];
 let sqrt = [];
+function oblicz(x) {
+    let y = 0;
+    let degree = parseFloat(document.getElementById("stopien").value);
+    if (degree > 0) {
+        for (let i = 0; i < degree; i++) {
+            y += list[i] * Math.pow(x, degree - i);
+        }
+    }
+    y += list[degree];
+    return y;
+}
 function result() {
+    document.getElementById("result").innerHTML = ""
     list = [];
     sqrt = [];
-    let str = document.getElementById("value").value;
-    let parts = str.split(/(?=[-+])/);
-
+    let wielomian = document.getElementById("value").value;
+    let parts = wielomian.split(/(?=[-+])/);
     for (let i = 0; i < parts.length; i++) {
         let part = parts[i];
-
         if (part.includes("x")) {
-            let coefficient = part;
-
-            if (coefficient === "x" || coefficient === "+x") {
-                coefficient = "+1x";
-            } else if (coefficient === "-x") {
-                coefficient = "-1x";
+            let znak = part;
+            if (znak === "x" || znak === "+x") {
+                znak = "+1x";
+            } else if (znak === "-x") {
+                znak = "-1x";
             }
-
             let prevChar = i > 0 ? parts[i - 1] : "";
             if (prevChar === "-") {
-                coefficient = "-" + coefficient
+                znak = "-" + znak
             }
-
-            let num = parseFloat(coefficient);
+            let num = parseFloat(znak);
             if (!isNaN(num)) {
                 list.push(num);
             }
@@ -34,52 +41,27 @@ function result() {
             }
         }
     }
-
     console.log(list);
     let from = parseFloat(document.getElementById("z").value);
-
     let to = parseFloat(document.getElementById("do").value);
-
     let iterator = parseFloat(document.getElementById("st").value);
-
-
     if (iterator !== 0) {
         for (let i = from; i < to; i += iterator) {
             let x1 = i;
             let x2 = i + iterator;
-
-            let y1 = calculate(x1);
-            let y2 = calculate(x2);
+            let y1 = oblicz(x1);
+            let y2 = oblicz(x2);
             console.log(y1)
             console.log(y2)
-
             if ((y1 < 0 && y2 >= 0) || (y1 >= 0 && y2 < 0)) {
                 sqrt.push((x1 + x2) / 2);
             }
         }
     }
+    if (sqrt.length > 0) {
 
-    if (sqrt.length > 0 ) {
-        for (let i = 0; i < sqrt.length; i++) {
-            document.getElementById("result").innerHTML += sqrt[i] + "\n";
-        }
-    }
-    else {
+        document.getElementById("result").innerHTML = sqrt.join(", ")
+    } else {
         document.getElementById("result").innerHTML = "Brak pierwiastków"
     }
 }
-
-    function calculate(x) {
-        let y = 0;
-
-        let degree = parseFloat(document.getElementById("stopien").value);
-
-        if (degree > 0) {
-            for (let i = 0; i < degree; i++) {
-                y += list[i] * Math.pow(x, degree - i);
-            }
-        }
-
-        y += list[degree];
-        return y;
-    }
